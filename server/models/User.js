31c609e0ef879e89ee0cc69b23e7fc4e6dff7f9b
Scott Erwin <scottallenerwin@gmail.com>
2021-07-17 11:30:46 -0400

    mearn set up

---
+++ server/models/User.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+    first_name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    last_name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    password_confirm: {
+        type: String,
+        required: true,
+    }
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
