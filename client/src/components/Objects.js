dc150d1c811397bc53ee3aaf860a9dc3419bcd92
Scott Erwin <scottallenerwin@gmail.com>
2021-07-21 16:26:20 -0400

    fixed media qs

--- client/src/components/Objects.js
+++ client/src/components/Objects.js
@@ -1,18 +1,21 @@
 // Header Backgound Changes Depending on Width
 export const headerbackground = `
-    @media only screen and (max-width: 650px)  {
-        .backimage {
-            background-image: url("images/header/abduction-Mobile.gif")
-        }
+    .backimage {
+        background-image: url("images/header/abduction-Mobile.gif")
     }
-    @media only screen and (min-width: 1024px) {
+    @media only screen and (min-width: 650px)  {
         .backimage {
             background-image: url("images/header/abduction-tablet.gif")
         }
     }
-    @media only screen and (min-width: 1920px) {
+    @media only screen and (min-width: 1024px) {
         .backimage {
-            background-image: url("images/header/abduction1920-937.gif")
+            background-image: url("abduction1920-937.gif")
+        }
+    }
+    @media only screen and (min-width: 1024px) and (min-width: 900px) {
+        .backimage {
+            background-image: url("images/header/abduction-1920-1200.gif")
         }
     }
     @media only screen and (min-width: 1920px) and (min-width: 1200px){
