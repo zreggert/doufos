194d3e06619a7c5e0303e845bb744ba886ba1d77
elgalloloco <zreggert@gmail.com>
2021-07-22 19:51:55 -0400

    modified app-signup and signup js

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations'
 
 
 import Auth from '../utils/auth';
@@ -14,7 +15,7 @@ const Signup = () => {
         password: '',
     });
 
-    const [addProfile, { error, data }] = useMutation(ADD_USER);
+    const [addUser] = useMutation(ADD_USER);
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -23,53 +24,71 @@ const Signup = () => {
             ...formState,
             [name]: value
         });
-    }
+    };
 
-    handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault(); 
-        const data = {
-            first_name: this.firstName,
-            last_name: this.lastName,
-            email: this.email,
-            password: this.password,
-            password_confirm: this.confirmPassword
-        };
-        
-        console.log(data);
-        //post request here to create new user in database
+        // const data = {
+        //     first_name: this.firstName,
+        //     last_name: this.lastName,
+        //     email: this.email,
+        //     password: this.password,
+        //     password_confirm: this.confirmPassword
+        // };
+
+        try {
+            const { data } = await addUser({
+                variables: { ...formState },
+            });
+            Auth.login(data.addUser.token);
+        } catch (err) {
+            console.log(err)
+        }
     };
 
     
     return (
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={handleSubmit}>
             <h3>Sign Up</h3>
 
             <div className="form-group">
                 <label>First Name</label>
-                <input type="text" className="form-control" placeholder="First Name" onChange={e => this.firstName = e.target.value} />
+                <input type="text" className="form-control" placeholder="First Name" value={formState.fistName} onChange={handleChange} 
+                // {e => this.firstName = e.target.value} 
+                />
             </div>
 
             <div className="form-group">
                 <label>Last Name</label>
-                <input type="text" className="form-control" placeholder="Last Name" onChange={e => this.lastName = e.target.value} />
+                <input type="text" className="form-control" placeholder="Last Name" value={formState.lastName} onChange={handleChange}
+                // {e => this.lastName = e.target.value} 
+                />
             </div>
 
             <div className="form-group">
                 <label>Email</label>
-                <input type="email" className="form-control" placeholder="Email" onChange={e => this.email = e.target.value} />
+                <input type="email" className="form-control" placeholder="Email" value={formState.email} onChange={handleChange}
+                // {e => this.email = e.target.value} 
+                />
             </div>
 
             <div className="form-group">
                 <label>Password</label>
-                <input type="password" className="form-control" placeholder="Password" onChange={e => this.password = e.target.value} />
+                <input type="password" className="form-control" placeholder="Password" value={formState.password} onChange={handleChange}
+                // {e => this.password = e.target.value} 
+                />
             </div>
 
             <div className="form-group">
                 <label>Confirm Password</label>
-                <input type="password" className="form-control" placeholder="Confirm Password" onChange={e => this.confirmPassword = e.target.value} />
+                <input type="password" className="form-control" placeholder="Confirm Password" value={formState.confirmPassword} onChange={handleChange}
+                // {e => this.confirmPassword = e.target.value} 
+                />
             </div>
 
             <button className="btn btn-primary btn-block">Sign Up</button>
         </form>
-    ) 
-}
+    ); 
+};
+
+export default Signup;
