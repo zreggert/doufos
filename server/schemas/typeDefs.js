2163a69fd81cb3944251f6648c17e2103970754e
elgalloloco <zreggert@gmail.com>
2021-07-23 15:18:27 -0400

    sightings seeded

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,6 +9,16 @@ const typeDefs = gql`
         password: String
     }
 
+    type Sighting {
+        _id: ID
+        latitude: Number
+        longitude: Number
+        city: String
+        state: String
+        date_time: Date
+        text: String
+    }
+
     type Auth {
         token: ID!
         user: User
@@ -17,11 +27,14 @@ const typeDefs = gql`
     type Query {
         users: [User]!
         user(userId: ID!): User
+        sightings: [Sighting]!
     }
+
 
     type Mutation {
         addUser(name: String!, email: String!, password: String!): Auth
         login(email: String!, password: String!): Auth
+        addSighting(latitude: Integer, longitude: Integer, city: String, state: String, date_time: Date, text: String): Sighting
     }
 `;
 
