8dd63b1aadb5faf2a2ef6f7bbba43e00c79c2159
Scott Erwin <scottallenerwin@gmail.com>
2021-07-25 14:19:03 -0400

    updated footer css

--- client/src/components/Navbot.js
+++ client/src/components/Navbot.js
@@ -5,16 +5,16 @@ export default function Navbot() {
   return (
     <nav className="botNav">
       <div className="bottomNav" id="bottomNav">
-        <p>
-          <b>SITE MAP</b>
+        <p className="botNavTitle">
+          <b className="titles">SITE MAP</b>
         </p>
         <a href="#test">Home</a>
         <a href="#test">Info</a>
         <a href="#test">Sightings</a>
       </div>
       <div className="social">
-        <p>
-          <b>SOCIAL</b>
+        <p className="botNavTitle">
+          <b className="titles">SOCIAL</b>
         </p>
         <p className="following">They're already following you!</p>
       </div>
